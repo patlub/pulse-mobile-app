c18de95603c8c861a2e51b20d7b37229d9c9a9e7
Descartess <nyondopaul23@gmail.com>
2018-04-04 12:42:35 +0300

    Add redux setup

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,15 +4,30 @@ import {
   View,
   Button
 } from 'react-native'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as counterActions from '../../actions'
 import styles from './styles'
 
 class Login extends Component {
   render() {
+    const { counter, counterActions } = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to Log In
         </Text>
+        <Text> 
+          {counter.count}
+        </Text>
+        <Button 
+          title="+"
+          onPress={counterActions.increment}
+        /> 
+        <Button 
+          title="-"
+          onPress={counterActions.decrement}
+        />
         <Button
           title="Log In"
           onPress={() =>
@@ -24,4 +39,13 @@ class Login extends Component {
   }
 }
 
-export default Login
+const mapStateToProps = (state) => {
+  const { counter } = state
+  return { counter }
+}
+
+const mapDispatchToProps = dispatch => ({
+  counterActions: bindActionCreators(counterActions,dispatch)
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login)
