9fed2e4c39147307510918be69b8461ce825b70d
Ivan Kipyegon <ivan.kipyegon@andela.com>
2018-04-06 17:51:28 +0300

    chore(redux-persist): implement redux-persist with navigation - configure store persisting to async storage - use Persistgate to persist data [Delivers #156371588]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import RootNavigator from './navigation/rootNavigator'
-import store from './config/store'
+import { PersistGate } from 'redux-persist/es/integration/react';
+import configureStore from './store/configureStore'
+import ApplicationWithNavigation from './navigation';
+
+
+const { store, persistor } = configureStore();
 
 
 const App = () => (
   <Provider store={store}>
-    <RootNavigator />
+    <PersistGate persistor={persistor}>
+      <ApplicationWithNavigation />
+    </PersistGate>
   </Provider>
 )
 
