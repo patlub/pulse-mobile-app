243699d33dcbb20b8c7315b712548a85d7cbb478
Ivan Kipyegon <ivan.kipyegon@andela.com>
2018-04-06 17:57:47 +0300

    refactor(reducer): add other base reducers for other components [Delivers #156371588]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,23 @@
+import { AsyncStorage } from 'react-native'
+import { persistCombineReducers } from 'redux-persist'
+import navigationReducer from '../navigation/reducer'
+import checkInReducer from '../components/CheckIn/reducer'
+import loginReducer from '../components/Login/reducer'
+import newCheckInReducer from '../components/NewCheckIn/reducer'
+import partnerHistoryReducer from '../components/PartnerHistory/reducer'
+import partnerListReducer from '../components/PartnerList/reducer'
 
-import { combineReducers } from 'redux'
-import counterReducer from './counterReducers'
 
-const reducers = combineReducers({
-    counter: counterReducer
-})
+const config = { key: 'root', storage: AsyncStorage }
 
-export default reducers
+
+const reducers = {
+  navigation: navigationReducer,
+  login: loginReducer,
+  checkIn: checkInReducer,
+  newCheckInReducer: newCheckInReducer,
+  partnerHistory: partnerHistoryReducer,
+  partnerList: partnerListReducer,
+}
+
+export default persistCombineReducers(config, reducers)
