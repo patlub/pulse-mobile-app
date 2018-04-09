240cce0c6b1de12a22558afcc4610d7b7344846a
sirjmkitavi <kitavimjoseph@gmail.com>
2018-04-09 21:16:11 +0300

    feat(): UI implementation for Login Screen

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react'
-import {
-  Text,
-  View,
-  Button
-} from 'react-native'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
+
 import styles from './styles'
 
 class Login extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Log In
-        </Text>
-        <Button
-          title="Log In"
-          onPress={() =>
-            this.props.navigation.navigate('Drawer')
-          }
-        />
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <Image source={require('../../assets/images/andela_logo.png')} style={styles.logoStyle} />
+          </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleStyle}>PULSE</Text>
+          </View>
+          <View style={styles.touchableContainer}>
+            <TouchableOpacity
+              style={styles.touchableStyle} activeOpacity={0.6}
+              onPress={() =>
+                this.props.navigation.navigate('PartnerList')
+              }
+            >
+              <View style={styles.touchableImageContainer} >
+                <Image source={ require('../../assets/images/google_logo.png')} style={styles.touchableImage} />
+              </View>
+              <View style={styles.touchableDivider}></View>
+              <View style={styles.touchableTextContainer}>
+                <Text style={styles.touchableText} >SIGN IN TO GET STARTED</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     )
   }
