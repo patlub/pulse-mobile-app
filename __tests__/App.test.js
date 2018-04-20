8868a06685665927a854bfa1482319dd920b8490
Ivan Kipyegon <ivan.kipyegon@andela.com>
2018-04-20 18:01:24 +0300

    chore(): Add setupFile for jest - mocks NativeEmitter module of react-native-safari-view [Delivers #156569762]

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -4,8 +4,15 @@ import App from '../src/App'
 
 import renderer from 'react-test-renderer'
 
+jest.mock('react-native-safari-view', () => {
+  return {
+    NativeEventEmitter: jest.fn(),
+  }
+})
+
 it('renders correctly', () => {
   const tree = renderer.create(
     <App />
   )
+  console.log(tree);
 })
--- setUpTests.js
+++ setUpTests.js
@@ -1,4 +1,12 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { NativeModules } from "react-native";
+
 
 Enzyme.configure({ adapter: new Adapter() })
+
+
+NativeModules.BlobModule = {
+  ...NativeModules.BlobModule,
+  addNetworkingHandler: jest.fn()
+};
