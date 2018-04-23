001f1dbb1de578a28a04cbcdf61f1183c9afed99
Julie Mugira <julie.mugira@andela.com>
2018-04-23 13:32:44 +0300

    tests(side-drawer): test that the onPress methods are working correctly

--- __tests__/components/SideDrawer.test.js
+++ __tests__/components/SideDrawer.test.js
@@ -1,19 +1,46 @@
 import 'react-native'
 import React from 'react'
 import { shallow } from 'enzyme'
+
 import SideDrawer from '../../src/components/common/SideDrawer'
 
 describe('Testing SideDrawer Component', () => {
+  const navigate = jest.fn()
+  const props = {
+    navigation: { navigate },
+  }
+
   it('renders as expected', () => {
     const wrapper = shallow(<SideDrawer />)
     expect(wrapper).toMatchSnapshot();
   })
 
   it('toggleShowLogout: should set state to new state', () => {
-    const wrapper = shallow(<SideDrawer showLogout={false} />)
-    const instance = wrapper.instance();
-    expect(instance.state.showLogout).toBe(false);
-    instance.toggleShowLogout();
-    expect(instance.state.showLogout).toBe(true);
+    const wrapper = shallow(<SideDrawer {...props} />)
+    const emailButton = wrapper.find('TouchableOpacity').at(0)
+    emailButton.simulate('Press')
+    expect(wrapper.state().showLogout).toBe(true)
+  })
+
+  it('navigates to Partners screen', () => {
+    const wrapper = shallow(<SideDrawer {...props} />)
+    const partnersNav = wrapper.find('TouchableOpacity').at(1)
+    partnersNav.simulate('Press');
+    expect(navigate).toBeCalled()
+  })
+
+  it('navigates to checkin screen', () => {
+    const wrapper = shallow(<SideDrawer {...props} />)
+    const checkinNav = wrapper.find('TouchableOpacity').at(2)
+    checkinNav.simulate('Press');
+    expect(navigate).toBeCalled()
+  })
+
+  it('navigates to logout', () => {
+    const wrapper = shallow(<SideDrawer {...props} />)
+    wrapper.setState({ showLogout: true })
+    const logoutNav = wrapper.find('TouchableOpacity').at(1)
+    logoutNav.simulate('Press');
+    expect(navigate).toBeCalled()
   })
 })
