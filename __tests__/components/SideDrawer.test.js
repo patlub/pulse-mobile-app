d6b5e89134b6b805c9f41cda77ad4452127b7d62
Patrick <patrick.luboobi@andela.com>
2018-05-14 14:48:20 +0300

    Fix() Fix props warning in SideDrawer test

--- __tests__/components/SideDrawer.test.js
+++ __tests__/components/SideDrawer.test.js
@@ -1,46 +1,45 @@
 import 'react-native'
 import React from 'react'
 import { shallow } from 'enzyme'
-
 import SideDrawer from '../../src/components/common/SideDrawer'
 
 describe('Testing SideDrawer Component', () => {
-  const navigate = jest.fn()
-  const props = {
-    navigation: { navigate },
+
+  navigation = {
+    navigate: jest.fn(),
   }
 
   it('renders as expected', () => {
-    const wrapper = shallow(<SideDrawer />)
+    const wrapper = shallow(<SideDrawer navigation={navigation}/>)
     expect(wrapper).toMatchSnapshot();
   })
 
   it('toggleShowLogout: should set state to new state', () => {
-    const wrapper = shallow(<SideDrawer {...props} />)
+    const wrapper = shallow(<SideDrawer navigation={navigation}/>)
     const emailButton = wrapper.find('TouchableOpacity').at(0)
     emailButton.simulate('Press')
     expect(wrapper.state().showLogout).toBe(true)
   })
 
   it('navigates to Partners screen', () => {
-    const wrapper = shallow(<SideDrawer {...props} />)
+    const wrapper = shallow(<SideDrawer navigation={navigation}/>)
     const partnersNav = wrapper.find('TouchableOpacity').at(1)
     partnersNav.simulate('Press');
-    expect(navigate).toBeCalled()
+    expect(navigation.navigate).toBeCalled()
   })
 
   it('navigates to checkin screen', () => {
-    const wrapper = shallow(<SideDrawer {...props} />)
+    const wrapper = shallow(<SideDrawer navigation={navigation}/>)
     const checkinNav = wrapper.find('TouchableOpacity').at(2)
     checkinNav.simulate('Press');
-    expect(navigate).toBeCalled()
+    expect(navigation.navigate).toBeCalled()
   })
 
   it('navigates to logout', () => {
-    const wrapper = shallow(<SideDrawer {...props} />)
-    wrapper.setState({ showLogout: true })
+    const wrapper = shallow(<SideDrawer navigation={navigation}/>)
+    wrapper.setState({showLogout: true})
     const logoutNav = wrapper.find('TouchableOpacity').at(1)
     logoutNav.simulate('Press');
-    expect(navigate).toBeCalled()
+    expect(navigation.navigate).toBeCalled()
   })
 })
